dd0ccbefb32b9d305299d3feaf7f6f17c0090562
oussamaom7 <om7.oussama@gmail.com>
2023-11-25 00:10:37 +0100

    yy

--- src/containers/product/ProductPage.jsx
+++ src/containers/product/ProductPage.jsx
@@ -5,23 +5,26 @@ import './pp.css'; // You can keep your custom styles if needed
 
 function ProductPage() {
   return (
-    <div className="flex items-center">
-      <div className="w-1/2 mr-4">
-        <img src={wac} alt="wacvssun" className="max-w-full h-auto" />
-      </div>
-      <div className="w-1/2 flex flex-col">
-        <Typography variant="h4" className="mb-4">LOGO</Typography>
-        <Typography variant="h4" className="mb-4">Product Name</Typography>
-        <Typography variant="body1" className="mb-4">Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
-        <Typography variant="h6" className="mb-4">Price: $19.99</Typography>
-        <Button variant="contained" color="primary" className="mb-4">
-          Add to Cart
-        </Button>
-        <div className="flex items-center">
-          <Typography variant="body1" className="mr-2">Quantity:</Typography>
-          <TextField type="number" variant="outlined" size="small" defaultValue={1} />
+    <div className="flex flex-col items-center">
+      <Typography variant="h3" className="mb-4">Product Details</Typography>
+      <div className="flex items-center justify-between">
+        <div className="w-1/2 mr-4">
+          <img src={wac} alt="wacvssun" className="max-w-full h-auto" />
+        </div>
+        <div className="w-1/2 flex flex-col">
+          <Typography variant="h4" className="mb-2">LOGO</Typography>
+          <Typography variant="h4" className="mb-2">Date of Event</Typography>
+          <Typography variant="body1" className="mb-2">Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+          <Typography variant="h6" className="mb-2">Price: $19.99</Typography>
+          <div className="flex items-center mb-2">
+            <Typography variant="body1" className="mr-2">Quantity:</Typography>
+            <TextField type="number" variant="outlined" size="small" defaultValue={1} />
+          </div>
         </div>
       </div>
+      <Button variant="contained" style={{ width: '200px' }} color="primary" size="medium" className="mt-4 p-2 self-end">
+        Add to Cart
+      </Button>
     </div>
   );
 }
